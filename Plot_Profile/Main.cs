9b8b8544069e1ac75fc5ccb964836a6ed38f570c
Georgi Danovski <georgi_danovski@abv.bg>
2020-04-06 14:38:02 +0300

    Add project files.

---
+++ Plot_Profile/Main.cs
@@ -0,0 +1,20 @@
+﻿using CellToolDK;
+
+public class Main
+{
+    private Transmiter t;
+    private TifFileInfo fi;
+
+    private void ApplyChanges()
+    {
+        //Apply changes and reload the image
+        t.ReloadImage();
+    }
+
+    public void Input(TifFileInfo fi, Transmiter t)
+    {
+        this.t = t;
+        this.fi = fi;
+        //Main entrance
+    }
+}
